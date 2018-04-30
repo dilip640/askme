52bc0d66b2154a076689b783607f47f9dca23582
dilip640 <2017csb1073@iitrpr.ac.in>
2018-04-30 13:34:22 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
-<?php $con=mysqli_connect("sql313.epizy.com","	epiz_22010858","dsplayer123","epiz_22010858_askme");
+<?php $con=mysqli_connect("sql313.epizy.com","epiz_22010858","dsplayer123","epiz_22010858_askme");
 
 if (mysqli_connect_errno())
 {
