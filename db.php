efda4c9e5b05d0a91651a2b25569950a378a6603
dilip640 <2017csb1073@iitrpr.ac.in>
2018-05-12 23:44:53 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
-<?php $con=new mysqli(null,'root','dsplayer123',"askme",null,"/cloudsql/askme8896:us-central1:root1");
+<?php $con=new mysqli(35.232.207.250,'root','dsplayer123',"askme",null,"/cloudsql/askme8896:us-central1:root1");
 
 if (mysqli_connect_errno())
 {
